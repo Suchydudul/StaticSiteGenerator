00eb70eb124339f3b5de8b1de037fa487c258888
SuchyDudul <dadudek99@protonmail.com>
2026-06-24 21:38:16 +0200

    Markdown blocks to blocktypes

--- src/markdown_to_blocks.py
+++ src/markdown_to_blocks.py
@@ -1,9 +1,47 @@
-def markdown_to_blocks(markdown):
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+
+def markdown_to_blocks(markdown: str) -> list[str]:
     splitted = markdown.split("\n\n")
-    print(splitted)
     result = []
     for i in splitted:
         i = i.strip()
         if len(i) > 0:
             result.append(i)
     return result
+
+
+def block_to_block_type(block: str) -> BlockType:
+    lines = block.split("\n")
+    if re.match(r"\#{1,6}\ .+", block):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
